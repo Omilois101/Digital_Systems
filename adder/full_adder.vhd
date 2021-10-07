0200cd61798b92b08dbb00f7e4746f3e9d76c4a5
Omilois101 <79018342+Omilois101@users.noreply.github.com>
2021-10-07 13:56:44 +0100

    Update full_adder.vhd

--- adder/full_adder.vhd
+++ adder/full_adder.vhd
@@ -1,1 +1,16 @@
-
+library IEEE; -- declartion of IEEE library 
+use IEEE.std_logic_1164.all; -- 
+  
+-- ENTITY
+  entity full_adder is 
+    port(a,b,cin:in std_logic;
+         s,cout:out std_logic;)
+  end full_adder;
+-- Architecture 
+  architecture structural of full_adder -- full_adder is the name but structural defines the full_adder
+  begin   -- add the begin 
+    s <= ( a xor b)xor cin ;
+    cout <= ((a and b) or ( a and cin ) or ( b and cin));
+  end structural;
+      
+  
